94b30d3b39807af4ba804ea2d6b7e63c07f9461d
minchik <minchik@gmail.com>
2020-04-23 10:57:51 +0300

    Calculate average number of steps to guess number for interval

--- module_0/main.py
+++ module_0/main.py
@@ -1,4 +1,8 @@
+from statistics import mean
+
 from module_0.algorithm import steps_to_guess
 
 if __name__ == "__main__":
-    print(steps_to_guess(2, (1, 100)))
+    guess_interval = (1, 100)
+    steps = [steps_to_guess(x, guess_interval) for x in range(guess_interval[0], guess_interval[1] + 1)]
+    print("Average number of steps for interval %s is %.2f" % (guess_interval, mean(steps)))
